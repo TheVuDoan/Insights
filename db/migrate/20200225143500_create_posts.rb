e2c9ecac5afdd5ac2e80ae5441cfc89fe50d3b8e
TheVuDoan <thevudoan@gmail.com>
2020-02-25 21:37:47 +0700

    create model Post

--- db/migrate/20200225143500_create_posts.rb
+++ db/migrate/20200225143500_create_posts.rb
@@ -2,10 +2,11 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.string :title
-      t.text :description
       t.datetime :publish_date
+      t.text :description
       t.string :source
       t.string :image
+      t.string :url
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_142456) do
+ActiveRecord::Schema.define(version: 2020_02_25_143500) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.text "description"
     t.datetime "publish_date"
+    t.text "description"
     t.string "source"
     t.string "image"
+    t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
