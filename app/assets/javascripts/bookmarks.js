8a52e3ba22944395940d1bc5deea2a4f1befe492
TheVuDoan <thevudoan@gmail.com>
2020-03-30 18:01:02 +0700

    Fix bookmark

--- app/assets/javascripts/bookmarks.js
+++ app/assets/javascripts/bookmarks.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
   $(function() {
     $('.card-bookmark').on('click', function() {
-      const post_id = $('.card-bookmark').data('post_id')
-      // $.post('/bookmarks', { post_id: post_id }, function() {
-        
-      // });
+      const post_id = $(this).data('post_id')
+      $.post('/bookmarks', { post_id: post_id }, function() {
+        location.reload()
+      });
     })
   })
 });
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -13,25 +13,26 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  def create
-    build_resource
+  # def create
+  #   build_resource
 
-    if resource.save
-      if resource.active_for_authentication?     
-        set_flash_message :notice, :signed_up if is_navigational_format?
-        sign_in(resource_name, resource)
-        respond_with resource, :location => redirect_location(resource_name, resource)
-      else
-        set_flash_message :notice, :inactive_signed_up, :reason => resource.inactive_message.to_s if is_navigational_format?
-        expire_session_data_after_sign_in!
-        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
-      end
-    else
-      clean_up_passwords(resource)
-      flash[:notice] = flash[:notice].to_a.concat resource.errors.full_messages
-      redirect_to '/users/sign_up/'
-    end
-  end
+  #   if resource.save
+  #     if resource.active_for_authentication?     
+  #       set_flash_message :notice, :signed_up if is_navigational_format?
+  #       sign_in(resource_name, resource)
+  #       respond_with resource, :location => redirect_location(resource_name, resource)
+  #     else
+  #       set_flash_message :notice, :inactive_signed_up, :reason => resource.inactive_message.to_s if is_navigational_format?
+  #       expire_session_data_after_sign_in!
+  #       respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+  #     end
+  #   else
+  #     debugger
+  #     clean_up_passwords(resource)
+  #     flash[:notice] = flash[:notice].to_a.concat resource.errors.full_messages
+  #     redirect_to '/users/sign_up/'
+  #   end
+  # end
 
   # GET /resource/edit
   # def edit
