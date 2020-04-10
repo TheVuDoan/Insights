303af4b301c28647da24d44e6b7da4ce0ecd9c2f
Doan The Vu <vu.doan@linkbal.co.jp>
2020-04-10 13:39:53 +0700

    add truncate des and title

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,20 @@
 module PostsHelper
+  DESCRIPTION_MAX_LENGTH = 125
+  TITLE_MAX_LENGTH = 50
+
+  def truncate_description(text)
+    short_text = if text.length > DESCRIPTION_MAX_LENGTH
+                   text.slice(0, DESCRIPTION_MAX_LENGTH).concat('...') 
+                 else
+                   text
+                 end
+  end
+
+  def truncate_title(text)
+    short_text = if text.length > TITLE_MAX_LENGTH
+                   text.slice(0, TITLE_MAX_LENGTH).concat('...') 
+                 else
+                   text
+                 end
+  end
 end
