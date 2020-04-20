42a190bcd4f63eeb8a944ea6e78d7b81857e05b8
Doan The Vu <vu.doan@linkbal.co.jp>
2020-04-20 11:05:08 +0700

    refactor css

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,7 +6,6 @@ $(document).on('turbolinks:load', function() {
   
   // Toggle the side navigation
   $("body").toggleClass("sidebar-toggled");
-  // $(".sidebar").toggleClass("toggled");
   $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
     $("body").toggleClass("sidebar-toggled");
     $(".sidebar").toggleClass("toggled");
