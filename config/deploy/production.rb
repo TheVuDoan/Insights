b7b1ada5079745cc1504f6cc6b993c8fa90b14b4
TheVuDoan <thevudoan@gmail.com>
2020-05-14 23:05:02 +0700

    fix uglifier

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,6 +59,7 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+config.assets.js_compressor = Uglifier.new(harmony: true)
 
 set :stage, :production
 set :rails_env, :production
